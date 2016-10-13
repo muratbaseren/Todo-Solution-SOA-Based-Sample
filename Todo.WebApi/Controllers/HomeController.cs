da81f44ce8348bac42d09b8066ff656249d122e1
kadirmuratbaseren <kadirmuratbaseren@gmail.com>
2016-10-13 10:34:02 +0300

    Web Api post data.. (AddTodoItem)

--- Todo.WebApi/Controllers/HomeController.cs
+++ Todo.WebApi/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Formatting;
 using System.Web.Http;
 using Todo.DataAccessLayer;
 using Todo.Entities;
@@ -15,6 +16,28 @@ namespace Todo.WebApi.Controllers
         {
             TodoContext db = new TodoContext();
             return db.TodoItems.ToList();
+        }
+
+        [HttpPost]
+        public bool AddTodoItem(FormDataCollection form)
+        {
+            TodoContext db = new TodoContext();
+            db.TodoItems.Add(
+                new TodoItem()
+                {
+                    Subject = form["subject"],
+                    Description = form["desc"],
+                    IsCompleted = false
+                });
+
+            if (db.SaveChanges() > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
--- Todo.WebMvc/Controllers/HomeController.cs
+++ Todo.WebMvc/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -20,5 +21,38 @@ namespace Todo.WebMvc.Controllers
 
             return View(items);
         }
+
+
+        public ActionResult Create()
+        {
+            return View(new TodoItem());
+        }
+
+        [HttpPost]
+        public ActionResult Create(TodoItem model)
+        {
+            WebClient wc = new WebClient() { Encoding = System.Text.Encoding.UTF8 };
+
+            NameValueCollection values = new NameValueCollection();
+            values.Add("subject", model.Subject);
+            values.Add("desc", model.Description);
+
+            var response = wc.UploadValues("http://localhost:51844/api/Home/AddTodoItem", values);
+
+            bool result = bool.Parse(System.Text.Encoding.UTF8.GetString(response));
+
+            if (result)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Hata oluştu.");
+                return View(model);
+            }
+
+        }
+
+
     }
 }
